513f645176469a1714e6196a88be9225f66b88fe
waqar-ahmad356 <flashdrivepunjab@gmail.com>
2024-02-13 14:27:31 +0500

    modified file

---
+++ index.js
@@ -0,0 +1,23 @@
+const express =require('express');
+const multer=require('multer');
+const app=express();
+
+const upload=multer({
+    storage:multer.diskStorage(
+        {
+            destination:function(req,file,cb)
+            {
+                cb(null,'uploads');
+            },
+            filename:function(req,file,cb)
+            {
+                cb(null,file.fieldname+"-"+Date.now()+".jpg");
+            }
+        }
+    )
+}).single('user-file');
+
+app.post('/upload',upload,(req,res)=>{
+    res.send('file upload');
+})
+app.listen(4500);
